3b711aa1e6c61ac112bb7bdc6e88385cab752490
chaitra <chaitra@LAPTOP-8FDBD6IT>
2023-01-12 20:12:03 +0530

    Automation code

---
+++ testNG/src/test/java/testNG/test/HomeTest.java
@@ -0,0 +1,20 @@
+package testNG.test;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+	@Test
+public void addToCart(){
+	System.out.println("inside addtoCraft test case");
+}
+	@Test
+	public void verifySearchbar() {
+		
+	System.out.println("inside verify searchbar test case");
+	}
+	@Test
+	public void login() {
+		System.out.println("");
+	}
+ 
+}
---
+++ testNG/src/test/java/testNG/test/LoginTest.java
@@ -0,0 +1,14 @@
+package testNG.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+@Test
+	public void username() {
+		System.out.println("Inside username method");
+}
+@Test
+public void password() {
+	System.out.println("inside password method");
+}
+}
